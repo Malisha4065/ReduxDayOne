1b6da9a0492a42099e02f2662e2bc7703ef57aa2
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2024-05-18 22:46:26 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import { createStore, compose, applyMiddleware, bindActionCreators } from "redux";
+
+const makeLouder = string => string.toUpperCase();
+const repeatThreeTimes = string => string.repeat(3);
+const embolden = string => string.bold();
+
+const makeLouderRepeatThreeTimesAndEmbolden = string => embolden(repeatThreeTimes(makeLouder(string)));
+
+console.log(makeLouderRepeatThreeTimesAndEmbolden('hello'));
+
+const makeLouderRepeatEmboldNew = compose(embolden, repeatThreeTimes, makeLouder);
+
+console.log(makeLouderRepeatEmboldNew('himom'));
