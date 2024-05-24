31dd19fc28f4df8d9ec8f181d3c9dfdd46c7ebc4
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2024-05-24 18:38:53 +0530

    middleware

--- 4-enhancers.js
+++ 4-enhancers.js
@@ -1,21 +1,48 @@
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 
-const reducer = state => state;
+const reducer = (state = { count: 1 }) => state;
 
 const monitorEnhancer = (createStore) => (reducer, initialState, enhancer) => {
-    const monitoredReducer = (state, action) => {
-        const start = performance.now();
-        const newState = reducer(state, action);
-        const end = performance.now();
-        const diff = end - start;
-        console.log(diff);
-        return newState;
-    };
+  const monitoredReducer = (state, action) => {
+    const start = performance.now();
+    const newState = reducer(state, action);
+    const end = performance.now();
+    const diff = end - start;
+    console.log(diff);
+    return newState;
+  };
 
-    return createStore(monitoredReducer, initialState, enhancer);    
+  return createStore(monitoredReducer, initialState, enhancer);
+};
+
+const logEnhancer = (createStore) => (reducer, initialState, enhancer) => {
+  const logReducer = (state, action) => {
+    console.log("old state", state, action);
+    const newState = reducer(state, action);
+    console.log("new state", newState, action);
+
+    return newState;
+  };
+  return createStore(logReducer, initialState, enhancer);
+};
+
+const logMiddleware = (store) => (next) => (action) => {
+    console.log('old state', store.getState(), action);
+    next(action);
+    console.log('new state', store.getState(), action);
 }
 
+const monitorMiddleware = (store) => (next) => (action) => {
+    const start = performance.now();
+    next(action);
+    const end = performance.now();
+    const diff = end - start;
+    console.log(diff);
+}
 
-const store = createStore(reducer, monitorEnhancer);
+// const store = createStore(reducer, monitorEnhancer);
+// const store = createStore(reducer, compose(logEnhancer, monitorEnhancer));
 
-store.dispatch({type: "Hello"});
+const store = createStore(reducer, applyMiddleware(logMiddleware, monitorMiddleware));
+
+store.dispatch({ type: "Hello" });
